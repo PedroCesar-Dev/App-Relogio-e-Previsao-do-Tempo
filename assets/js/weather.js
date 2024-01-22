42232211999fc7dd59989c56653b807fe9a8ba35
Pedro Cesar <119058413+PedroCesar-Dev@users.noreply.github.com>
2024-01-22 11:31:30 -0300

    update

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -7,10 +7,11 @@ function pesquisar() {
 let relogio;
 async function dados(cidade) {
     let dadosWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response => Response.json());
-    inserirDados(dadosWeather);
-    clearInterval(relogio);
-
-    relogio = setInterval(function time() {
+    console.log(dadosWeather);
+    if (dadosWeather.name != undefined ){
+        inserirDados(dadosWeather);
+        clearInterval(relogio);
+        relogio = setInterval(function time() {
         const dataAtual = new Date();
         const timezoneOffset = dadosWeather.timezone / 3600;
         const localTime = new Date(dataAtual.getTime() + timezoneOffset * 60 * 60 * 1000);
@@ -29,22 +30,33 @@ async function dados(cidade) {
             
         }
 
-        horasElement.innerHTML = horas;
-        minutosElement.innerHTML = minutos;
-        segundosElement.innerHTML = segundos; 
-            
-
+        horasElement.innerHTML = "Horas: " + horas;
+        minutosElement.innerHTML = "Minutos: " + minutos;
+        segundosElement.innerHTML = "Segundos: " + segundos; 
     })
-
+    }else{
+        alert("Por favor digite uma cidade valida!");
+    }
+    
+    
 }
 
 function inserirDados(dados) {
 
-    document.querySelector('.titulo-previsao').innerHTML = "Cidade: " + dados.name;
-    document.querySelector('.graus').innerHTML = "Temperatura: " + Math.floor(dados.main.temp) + "°C";
-    document.querySelector('.texto-previsao').innerHTML = "Clima: " + dados.weather[0].description;
-    document.querySelector('.umidade').innerHTML = "Umidade: " + dados.main.humidity + "%";
-    document.querySelector('.img-previsao').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+    document.querySelector('.selectedCity').innerHTML = "Cidade: " + dados.name;
+    document.querySelector('.selectedCountry').innerHTML = "País: " + dados.sys.country;
+    document.querySelector('.selectedTimeZone').innerHTML = "Fuso horário: " + (dados.timezone/3600);
+    document.querySelector('.weatherDescription').innerHTML = "Descrição: " + dados.weather[0].description;
+    document.querySelector('.img-previsao').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`;
+    document.querySelector('.temp').innerHTML = "Temperatura: " + Math.floor(dados.main.temp) + "°C";    
+    document.querySelector('.temp-Min').innerHTML = "Temperatura minima: " + Math.floor(dados.main.temp_min) + "°C";    
+    document.querySelector('.temp-Max').innerHTML = "Temperatura maxima: " + Math.floor(dados.main.temp_max) + "°C";    
+    document.querySelector('.humidity').innerHTML = "Umidade: " + dados.main.humidity + "%";
+    document.querySelector('.tempFeeling').innerHTML = "Sensação termica: " + Math.floor(dados.main.feels_like) + "°C";
+    document.querySelector('.cloudAmount').innerHTML = "Nuvens: " + dados.clouds.all + "%";
+    document.querySelector('.windSpeed').innerHTML = "Velocidade: " + dados.wind.speed + "m/s";
+    document.querySelector('.windDirection').innerHTML = "Direção: " + dados.wind.deg + "°";
+
     document.body.style.background = `url(https://source.unsplash.com/1600x900/?${dados.weather[0].main})`;
     document.body.style.backgroundSize = 'cover';
 }
